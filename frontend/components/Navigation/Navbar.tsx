88713d0a9e2e56c2ef3c8ebabe0b3b8c4be9c87a
Adam Topczewski <gnomowisko@gmail.com>
2023-08-01 20:06:48 +0200

    prettier: added library and ignore file; formated frontend

--- frontend/components/Navigation/Navbar.tsx
+++ frontend/components/Navigation/Navbar.tsx
@@ -1,19 +1,19 @@
-import NavLink from "./NavLink"
+import NavLink from "./NavLink";
 
 export default function Navbar() {
-    return (
-        <nav>
-            <ul className="flex gap-6 text-sm">
-                <li>
-                    <NavLink />
-                </li>
-                <li>
-                    <NavLink to="posts" label="Posty" />
-                </li>
-                <li>
-                    <NavLink to="about" label="O mnie" />
-                </li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav>
+      <ul className="flex gap-6 text-sm">
+        <li>
+          <NavLink />
+        </li>
+        <li>
+          <NavLink to="posts" label="Posty" />
+        </li>
+        <li>
+          <NavLink to="about" label="O mnie" />
+        </li>
+      </ul>
+    </nav>
+  );
 }
--- frontend/components/Shared/Footer.tsx
+++ frontend/components/Shared/Footer.tsx
@@ -2,13 +2,14 @@ import { AUTHORS_LAST_NAME } from "@/constants/Shared";
 import { AUTHORS_FIRST_NAME } from "@/constants/Shared";
 
 export default function Footer() {
-    return (
-        <footer className="w-full border-dashed border-y-2">
-            <div className="container mx-auto flex justify-center items-center text-sm py-4">
-                <p className="flex h-fit">
-                    <span className="text-base font-bold">© </span>
-                    {AUTHORS_FIRST_NAME + ' ' + AUTHORS_LAST_NAME}. All rights reversed.</p>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="w-full border-dashed border-y-2">
+      <div className="container mx-auto flex justify-center items-center text-sm py-4">
+        <p className="flex h-fit">
+          <span className="text-base font-bold">© </span>
+          {AUTHORS_FIRST_NAME + " " + AUTHORS_LAST_NAME}. All rights reversed.
+        </p>
+      </div>
+    </footer>
+  );
 }
